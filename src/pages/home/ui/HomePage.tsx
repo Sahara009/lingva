ad28eceb108319a7c3b74c312aa13d146cff5557
Sahara <lemonmugi@gmail.com>
2025-11-19 18:39:31 +0300

    add main section + styles + adaptive

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { MainSection } from "../../../widgets/MainSection/MainSection";
 
 interface Props {
   className?: string;
 }
 
 export const HomePage: React.FC<Props> = ({ className }) => {
-  return <div className={className}></div>;
+  return (
+    <div className={className}>
+      <MainSection />
+    </div>
+  );
 };
---
+++ src/widgets/MainSection/MainSection.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import styles from "./MainSection.module.scss";
+import { MoveUpRight } from "lucide-react";
+import cat1 from "../../shared/assets/cat1.png";
+import cat2 from "../../shared/assets/cat4.png";
+
+interface Props {
+  className?: string;
+}
+
+export const MainSection: React.FC<Props> = ({ className }) => {
+  const [activeCat, setActiveCat] = useState(0);
+  const cats = [cat1, cat2];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveCat((prev) => (prev === 0 ? 1 : 0));
+    }, 1000); // 1 секунда
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={className}>
+      <div className={styles.mainSection}>
+        <div className={styles.mainSection__text}>
+          <h1>
+            Изучайте иностранные <br /> языки с <span>международной</span>
+            <br />
+            онлайн-академией!
+          </h1>
+          <p>
+            «Библиотека Лингва» — образовательная платформа, объединяющая
+            обучение языкам, профильную литературу и программы погружения за
+            рубежом. Всё, что нужно для уверенного освоения языка — в одном
+            месте.
+          </p>
+          <div className={styles.mainSection__buttons}>
+            <button className={styles.mainSection__buttons_blue}>
+              Узнать больше
+            </button>
+            <button className={styles.mainSection__buttons_red}>
+              Подробнее <MoveUpRight className={styles.icon} />
+            </button>
+          </div>
+        </div>
+
+        <div className={styles.mainSection__photo}>
+          <div className={styles.block}></div>
+
+          <img
+            key={activeCat}
+            src={cats[activeCat]}
+            alt="cat"
+            className={`${styles.cat} ${styles.fadeCat}`}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
