49f742e969e77ff9bd8aaa06fb5bc9ec31d4928e
Sahara <lemonmugi@gmail.com>
2025-12-08 13:07:57 +0300

    fix how it works

--- src/pages/intership/ui/Intership.tsx
+++ src/pages/intership/ui/Intership.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./Intership.module.scss";
-import { HeroSection } from "../../../widgets/InterShipHero/IntershipHero";
+
 import { HowItWorks } from "../../../widgets/HowItWorks/HowItWorks";
 import { CountriesSection } from "../../../widgets/CountriesSection/CountriesSection";
 import { BenefitsSection } from "../../../widgets/BenefitsSection/BenefitsSection";
@@ -8,7 +8,6 @@ import { IntershipReviews } from "../../../widgets/IntershipReviews/IntershipRev
 import { FAQSection } from "../../../widgets/FAQSection/FAQSection";
 import { CTAApplySection } from "../../../widgets/CTAApplySection/CTAApplySection";
 import { CompareCountriesSection } from "../../../widgets/CompareCountriesSection/CompareCountriesSection";
-import JourneyTimeline from "../../../widgets/JourneyTimeline/JourneyTimeline";
 
 interface Props {
   className?: string;
@@ -18,7 +17,6 @@ export const Intership: React.FC<Props> = ({ className }) => {
   return (
     <div className={className}>
       <div className={style.intership}>
-        <HeroSection />
         <HowItWorks />
         <CountriesSection />
         <BenefitsSection />
